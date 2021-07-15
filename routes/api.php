6fdb56764b77ec47e218cc5ddeced9bce0b01077
mlfernandez <mlfernandez@gmail.com>
2021-07-15 17:29:21 +0200

    crud party controller

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PassportAuthController;
-use App\Http\Controllers\PostController;
+use App\Http\Controllers\PartyController;
 use App\Http\Controllers\UserController;
 
 /*
