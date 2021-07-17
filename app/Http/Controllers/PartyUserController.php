4422a06161df06672bc381d652c21b609ed478be
mlfernandez <mlfernandez@gmail.com>
2021-07-17 20:04:50 +0200

    partyuser delete

--- app/Http/Controllers/PartyUserController.php
+++ app/Http/Controllers/PartyUserController.php
@@ -158,7 +158,7 @@ class PartyUserController extends Controller
 
        if($user->id === $userInParty){  
 
-            $resultado = PartyUser::where('id', '=', $id)->get();
+            $resultado = PartyUser::where('id', '=', $id);
 
             if (!$resultado) {
                 return response() ->json([
